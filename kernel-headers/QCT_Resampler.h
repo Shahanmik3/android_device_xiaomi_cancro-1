a960586ecc9eb5e224b89b4c7a2b47b9b5508e62
Shahan-mik3 <Shahan.mike13@gmail.com>
2016-02-13 13:22:08 +0000

    Du bring up: lets make the rainbow appear

--- kernel-headers/QCT_Resampler.h
+++ kernel-headers/QCT_Resampler.h
@@ -14,8 +14,8 @@ class QCT_Resampler {
 
 public:
 	static size_t	MemAlloc(int bitDepth, int inChannelCount, int32_t inSampleRate, int32_t sampleRate);
-	static void		Init(int16_t *pState, int32_t inChannelCount, int32_t inSampleRate, int32_t mSampleRate);
-	static void		Resample90dB(int16_t* pState, int16_t* in, int32_t* out, size_t inFrameCount, size_t outFrameCount);
+	static void		Init(int16_t *pState, int32_t inChannelCount, int32_t inSampleRate, int32_t mSampleRate, int a);
+	static void		Resample90dB(int16_t* pState, int32_t* in, int32_t* out, size_t inFrameCount, size_t outFrameCount);
 	static size_t	GetNumInSamp(int16_t* pState, size_t outFrameCount);
 };
 };
